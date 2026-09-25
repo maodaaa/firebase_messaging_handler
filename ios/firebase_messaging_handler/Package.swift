655dbb2804d3694f46d6e71ce3731605df77a251
Maorid <gmaorid@gmail.com>
2026-09-26 05:14:19 +0700

    chore: upgrade minimum ios

--- ios/firebase_messaging_handler/Package.swift
+++ ios/firebase_messaging_handler/Package.swift
@@ -6,21 +6,22 @@ import PackageDescription
 let package = Package(
     name: "firebase_messaging_handler",
     platforms: [
-        .iOS("13.0"),
+        .iOS("15.0"),
     ],
     products: [
         .library(name: "firebase-messaging-handler", targets: ["firebase_messaging_handler"]),
     ],
     dependencies: [
-        .package(name: "FlutterFramework", path: "../FlutterFramework"),
+        .package(name: "FlutterFramework", path: "../FlutterFramework")
     ],
     targets: [
         .target(
             name: "firebase_messaging_handler",
             dependencies: [
-                .product(name: "FlutterFramework", package: "FlutterFramework"),
+                .product(name: "FlutterFramework", package: "FlutterFramework")
             ],
             path: "Sources/firebase_messaging_handler"
         ),
     ]
 )
+
